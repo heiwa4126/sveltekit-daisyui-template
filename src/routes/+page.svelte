a44cab72c0ae6a59a7e4a1e0a51f68b11cadb1a7
heiwa4126 <heiwa4126@gmail.com>
2023-07-10 18:03:44 +0900

    add range example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,9 @@
+<script>
+    let value =50;
+</script>
+
 <button class="btn">Hello daisyUI</button>
+<div>
+  <div>value={value}</div>
+  <input type="range" min={0} max={100} bind:value class="range w-48" />
+</div>
